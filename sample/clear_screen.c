e157cd990d8aa66af30cade23cf3a416914b3820
Tengu712 <genreihoutengu@gmail.com>
2023-04-03 23:17:28 +0900

    fix: clear_screen sample

--- sample/clear_screen.c
+++ sample/clear_screen.c
@@ -2,7 +2,7 @@
 
 int main() {
     ss_init_logger();
-    ss_init("clear screen", 640, 480, 640.0f, 480.0f, 0);
+    ss_init("clear screen", 640, 480, 0);
     while (1) {
         if (ss_should_close()) break;
         ss_render(0.2f, 0.2f, 0.2f, NULL, 0);
