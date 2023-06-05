4873cb19fc3d07e732611f43f63c0847f83ff3f4
Tengu712 <genreihoutengu@gmail.com>
2023-06-05 20:49:15 +0900

    docs: for crate.io

---
+++ examples/simple.rs
@@ -0,0 +1,14 @@
+use sstar::{
+    vulkan::*,
+    window::*,
+};
+
+fn main() {
+    let mut window_app = WindowApp::new("Sample Program\0", 640, 480);
+    let vulkan_app = VulkanApp::new(&window_app, 10);
+    while window_app.do_events() {
+        vulkan_app.render(None, &[]).unwrap();
+    }
+    vulkan_app.terminate();
+    window_app.terminate();
+}
