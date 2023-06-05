6745a035fd2ed6ecb7fd4065e6f12e9da10a4883
Tengu712 <genreihoutengu@gmail.com>
2023-06-06 01:03:41 +0900

    docs: for crates.io

--- README.md
+++ README.md
@@ -4,6 +4,26 @@
 
 A small frontend framework for game development. This framework is supported on Windows and Linux.
 
+# Example
+
+Here is an example that creates a window and clears it default color:
+
+```rust
+use sstar::{
+    vulkan::*,
+    window::*,
+};
+fn main() {
+    let mut window_app = WindowApp::new("Sample Program\0", 640, 480);
+    let vulkan_app = VulkanApp::new(&window_app, 10);
+    while window_app.do_events() {
+        vulkan_app.render(None, &[]).unwrap();
+    }
+    vulkan_app.terminate();
+    window_app.terminate();
+}
+```
+
 ## Build
 
 Following commands are required:
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,25 @@
+//! Shooting Star is a front engine for a simple 2D game.
+//! 
+//! # Example
+//! 
+//! Here is an example that creates a window and clears it default color:
+//! 
+//! ```rust
+//! use sstar::{
+//!     vulkan::*,
+//!     window::*,
+//! };
+//! fn main() {
+//!     let mut window_app = WindowApp::new("Sample Program\0", 640, 480);
+//!     let vulkan_app = VulkanApp::new(&window_app, 10);
+//!     while window_app.do_events() {
+//!         vulkan_app.render(None, &[]).unwrap();
+//!     }
+//!     vulkan_app.terminate();
+//!     window_app.terminate();
+//! }
+//! ```
+
 pub mod bitmap;
 pub mod log;
 #[allow(non_upper_case_globals)]
